5358809bf6d7d1c9e22dec4d2096f62e16ead5c9
Louis Lomboy <124099680+HrothgarFF14@users.noreply.github.com>
2023-10-23 08:15:00 -0700

    Initial commit

---
+++ Sort.java
@@ -0,0 +1,242 @@
+import java.util.*;
+
+
+/**
+ * A class that contains a group of sorting algorithms.
+ * The input to the sorting algorithms is assumed to be
+ * an array of integers.
+ * 
+ * @author Donald Chinn
+ * @version September 19, 2003
+ */
+public class Sort {
+
+    // Constructor for objects of class Sort
+    public Sort() {
+    }
+
+
+    /**
+     * Given an array of integers and an integer k, sort the array
+     * (ascending order) using k-way mergesort.
+     * @param data  an array of integers
+     * @param k     the k in k-way mergesort
+     */
+    public static void kwayMergesort (int[] data, int k) {
+        kwayMergesortRecursive (data, 0, data.length - 1, k);
+    }
+    
+    /**
+     * The recursive part of k-way mergesort.
+     * Given an array of integers (data), a low index, high index, and an integer k,
+     * sort the subarray data[low..high] (ascending order) using k-way mergesort.
+     * @param data  an array of integers
+     * @param low   low index
+     * @param high  high index
+     * @param k     the k in k-way mergesort
+     */
+    public static void kwayMergesortRecursive (int[] data, int low, int high, int k) {
+        if (low < high) {
+            for (int i = 0; i < k; i++) {
+                kwayMergesortRecursive (data,
+                                        low + i*(high-low+1)/k,
+                                        low + (i+1)*(high-low+1)/k - 1,
+                                        k);
+            }
+            merge (data, low, high, k);
+        }
+    }
+    
+
+    /**
+     * Given an array of integers (data), a low index, a high index, and an integer k,
+     * sort the subarray data[low..high].  This method assumes that each of the
+     * k subarrays  data[low + i*(high-low+1)/k .. low + (i+1)*(high-low+1)/k - 1],
+     * for i = 0..k-1, are sorted.
+     * @param data  an array of integers
+     * @param high  the high index
+     * @param k     the k in k-way mergesort
+     * @param low   the low index
+     */
+    public static void merge (int[] data, int low, int high, int k) {
+    
+        if (high < low + k) {
+            // the subarray has k or fewer elements
+            // just make one big heap and do deleteMins on it
+            Comparable[] subarray = new MergesortHeapNode[high - low + 1];
+            for (int i = 0, j = low; i < subarray.length; i++, j++) {
+                subarray[i] = new MergesortHeapNode(data[j], 0);
+            }
+            BinaryHeap heap = BinaryHeap.buildHeap(subarray);
+            for (int j = low; j <= high; j++) {
+                try {
+                    data[j] = ((MergesortHeapNode) heap.deleteMin()).getKey();
+                }
+                catch (EmptyHeapException e) {
+                    System.out.println ("Tried to delete from an empty heap.");
+                }
+            }
+            
+        } else {
+        // divide the array into k subarrays and do a k-way merge
+
+        //divide the array into k subarrays
+        //size of each subarray
+        // taking the top value index - low value index + 1 and then divide the size by k
+        int subArraySize = (high - low + 1)/k;
+        //Keep track of the start and end index of the subarrays
+        int[] subArrayStartIndex = new int[k];
+        int[] subArrayEndIndex = new int[k];
+
+        //calculate the starting and ending index for each subarray
+        for (int i = 0; i < k;i++)
+        {
+            subArrayStartIndex[i] = (i * subArraySize) + low;
+            subArrayEndIndex[i] = (subArrayStartIndex[i] + subArraySize) - 1;
+        }
+        //adjust the ending index for the last subarray to ensure all elements are covered
+        subArrayEndIndex[k-1] = high;
+        //create a binary heap to store the smallest elements in each subarray
+        BinaryHeap heap = new BinaryHeap();
+        //Initialize the heap with the smallest elements from each subarray
+        for (int i = 0; i < k; i++)
+        {
+            //Compare values in start and end index subarrays
+            if (subArrayStartIndex[i] <= subArrayEndIndex[i])
+            {
+                int value = data[subArrayStartIndex[i]];
+                //Insert the smallest element from each subarray into the heap
+                heap.insert(new MergesortHeapNode(value, i));
+                //Move pointer to the next element in the subarray
+                subArrayStartIndex[i]++;
+            }
+        }
+        //Merge the subarrays into the original array, base off the 'if' statement
+            for (int i = low; i <= high; i++)
+            {
+                try
+                {
+                    MergesortHeapNode node = (MergesortHeapNode) heap.deleteMin();
+                    int subarrayIndex = node.getWhichSubarray();
+                    int val = node.getKey();
+                    // Update the current position in the original array with the smallest value
+                    data[i] = val;
+                    if (subArrayStartIndex[subarrayIndex] <= subArrayEndIndex[subarrayIndex])
+                    {
+                        int nextVal = data[subArrayStartIndex[subarrayIndex]];
+                        //Insert the next smallest element from the same subarray to the heap
+                        heap.insert(new MergesortHeapNode(nextVal, subarrayIndex));
+                        subArrayEndIndex[subarrayIndex]++;
+                    }
+                }
+                //Check if there are more elements in the subarray to insert into the heap
+                catch (EmptyHeapException e)
+                {
+                    System.out.println("Tried to delete from an empty heap.");
+                }
+            }
+        }
+
+    }
+    
+    
+    /**
+     * Given an integer size, produce an array of size random integers.
+     * The integers of the array are between 0 and size (inclusive) with
+     * random uniform distribution.
+     * @param size  the number of elements in the returned array
+     * @return      an array of integers
+     */
+    public static int[] getRandomArrayOfIntegers(int size) {
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = (int) ((size + 1) * Math.random());
+        }
+        return data;
+    }
+    
+
+    /**
+     * Given an integer size, produce an array of size random integers.
+     * The integers of the output array are between 0 and size-1 with
+     * exactly one of each in the array.  Each permutation is generated
+     * with random uniform distribution.
+     * @param size  the number of elements in the returned array
+     * @return      an array of integers
+     */
+    public static int[] getRandomPermutationOfIntegers(int size) {
+        int[] data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = i;
+        }
+        // shuffle the array
+        for (int i = 0; i < size; i++) {
+            int temp;
+            int swap = i + (int) ((size - i) * Math.random());
+            temp = data[i];
+            data[i] = data[swap];
+            data[swap] = temp;
+        }
+        return data;
+    }
+
+
+    /**
+     * Perform checks to see if the algorithm has a bug.
+     */
+    private static void testCorrectness() {
+        int[] data = getRandomPermutationOfIntegers(100);
+        
+        for (int i = 0; i < data.length; i++) {
+            System.out.println("data[" + i + "] = " + data[i]);
+        }
+        
+        int k = 100;
+        kwayMergesort(data, k);
+        
+        // verify that data[i] = i
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] != i) {
+                System.out.println ("Error!  data[" + i + "] = " + data[i] + ".");
+            }
+        }
+    }
+    
+    
+    /**
+     * Perform timing experiments.
+     */
+    private static void testTiming () {
+        // timer variables
+        long totalTime = 0;
+        long startTime = 0;
+        long finishTime = 0;
+
+        // start the timer
+        Date startDate = new Date();
+        startTime = startDate.getTime();
+
+        int n = 100;    // n = size of the array
+        int k = 3;         // k = k in k-way mergesort
+        int[] data = getRandomArrayOfIntegers(n);
+        kwayMergesort(data, k);
+
+        // stop the timer
+        Date finishDate = new Date();
+        finishTime = finishDate.getTime();
+        totalTime += (finishTime - startTime);
+        
+        System.out.println("** Results for k-way mergesort:");
+        System.out.println("    " + "n = " + n + "    " + "k = " + k);
+        System.out.println("    " + "Time: " + totalTime + " ms.");
+    }
+    
+    
+    /**
+     * code to test the sorting algorithms
+     */
+    public static void main (String[] argv) {
+        testCorrectness();
+        testTiming();
+    }
+}
